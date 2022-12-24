20c3982b13892a6dd6cb89ade73f3b1cff57703d
keyiflerolsun <keyiflerolsun@gmail.com>
2022-12-24 23:21:24 +0300

    🕊
    🕊

--- Libs/__init__.py
+++ Libs/__init__.py
@@ -2,15 +2,17 @@
 
 from Libs.TCDD_Selenium import TCDD
 from notifypy           import Notify
+from os                 import name as sistem
 
 def bildirim(baslik:str, icerik:str):
 
     _bildirim = Notify()
     _bildirim._notification_application_name = "TCDD | @KekikAkademi"
     _bildirim._notification_icon             = "Resimler/TCDD.png"
+    if sistem == "nt":
+        _bildirim.icon = "Resimler/TCDD.png"
 
     _bildirim.title   = baslik
     _bildirim.message = icerik
-    _bildirim.icon    = "Resimler/TCDD.png"
 
     _bildirim.send(block=False)
